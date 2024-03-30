0e81d8877b75486594efc72adbf850d8569d9b73
Antonov Denis <den_ru1982@mail.ru>
2024-03-30 22:54:57 +0300

    Add updatePlusMinus + tests + updateDoubleMinus

--- Orchestrator/internal/tasks/arithmetic/upgrader.go
+++ Orchestrator/internal/tasks/arithmetic/upgrader.go
@@ -2,95 +2,9 @@ package arithmetic
 
 // Обновление выражения с добавлением скобок
 func Upgrade(exp []byte) []byte {
-	operatorIndex := 0
-	result := make([]byte, 0)
-	left := 0
-	for i := 0; i < len(exp); i++ {
-		if exp[i] == '+' || exp[i] == '-' {
-			if exp[operatorIndex] == '+' || exp[operatorIndex] == '-' {
-				array := append([]byte{'('}, exp[left:i]...)
-				array = append(array, ')')
-				array = append(array, exp[i])
-				result = append(result, array...)
-				left = i + 1
-				operatorIndex = 0
-				continue
-			}
-			operatorIndex = i
-			continue
-		}
-		/* if exp[i] == '*' || exp[i] == '/' {
-			if operatorIndex > left {
-				result = append(result, exp[left:operatorIndex+1]...)
-				left = operatorIndex + 1
-			}
-			array, index := upgradeMultiDivide(exp, i+1, left)
-			result = append(result, array...)
-			if index == len(exp) {
-				return result
-			}
-			operatorIndex = 0
-			if exp[index] == ')' {
-				return result
-			}
-			if exp[index] == '(' {
-				left = index
-				i = index - 1
-				continue
-			}
-			i = index
-			left = i + 1
-			if index == len(exp) {
-				break
-			}
-			result = append(result, exp[index])
-			continue
-		} */
-		if exp[i] == '(' {
-			result = append(result, exp[left:i+1]...)
-			array := Upgrade(exp[i+1:])
-
-			result = append(result, array...)
-			closeBoarder := -1
-			for j := i + 1; j < len(exp); j++ {
-				if exp[j] == ')' {
-					closeBoarder++
-				}
-				if exp[j] == '(' {
-					closeBoarder--
-				}
-				if closeBoarder == 0 {
-					i = j
-					break
-				}
-			}
-			result = append(result, exp[i])
-			if i == len(exp)-1 {
-				return result
-			}
-			if exp[i+1] == '+' || exp[i+1] == '-' ||
-				exp[i+1] == '*' || exp[i+1] == '/' {
-				i = i + 1
-				result = append(result, exp[i])
-			}
-			left = i + 1
-			operatorIndex = 0
-			continue
-		}
-		if exp[i] == ')' {
-			result = append(result, exp[left:i]...)
-			return result
-		}
-	}
-	if operatorIndex != 0 {
-		array := append([]byte{'('}, exp[left:]...)
-		array = append(array, ')')
-		result = append(result, array...)
-	} else {
-		result = append(result, exp[left:]...)
-	}
-
-	return result
+	exp = updateDoubleMinus(exp)
+	exp = upgradeMultiDivide(exp)
+	return upgradePlusMinus(exp)
 }
 
 // Добавляем скобки в умножение и деление
@@ -99,51 +13,38 @@ func upgradeMultiDivide(exp []byte) []byte {
 	left := 0
 	prevOperator := byte(0)
 	countBrackets := 0
-	if exp[0] == '(' {
-		i := 0
-		countBrackets++
-		result = append(result, '(')
-		array := upgradeMultiDivide(exp[1:])
-		for countBrackets > 0 {
-			i++
-			if exp[i] == '(' {
-				countBrackets++
-			}
-			if exp[i] == ')' {
-				countBrackets--
-			}
-		}
-		result = append(result, array...)
-		result = append(result, ')')
-		left = i + 1
-		prevOperator = 1
-	}
+
 	for i := left; i < len(exp); i++ {
+		if exp[i] == '(' {
+			countBrackets++
+			result = append(result, exp[left:i]...)
+			result = append(result, '(')
+			array := upgradeMultiDivide(exp[i+1:])
+			for countBrackets > 0 {
+				i++
+				if exp[i] == '(' {
+					countBrackets++
+				}
+				if exp[i] == ')' {
+					countBrackets--
+				}
+			}
+			i++
+			result = append(result, array...)
+			result = append(result, ')')
+			left = i
+			if i < len(exp) {
+				result = append(result, exp[i])
+				left = i + 1
+			}
+			prevOperator = 0
+			continue
+		}
 		if exp[i] == '*' {
 			if prevOperator == 1 {
 				result = append(result, exp[i])
 				left = i + 1
 				prevOperator = 0
-				continue
-			}
-			if exp[i+1] == '(' {
-				countBrackets++
-				i++
-				result = append(result, exp[left:i+1]...)
-				array := upgradeMultiDivide(exp[i+1:])
-				for countBrackets > 0 {
-					i++
-					if exp[i] == '(' {
-						countBrackets++
-					}
-					if exp[i] == ')' {
-						countBrackets--
-					}
-				}
-				result = append(result, array...)
-				result = append(result, ')')
-				left = i + 1
-				prevOperator = 1
 				continue
 			}
 			if prevOperator == 0 {
@@ -205,6 +106,117 @@ func upgradeMultiDivide(exp []byte) []byte {
 		result = append(result, exp[left:]...)
 		result = append(result, ')')
 		return result
+	}
+	result = append(result, exp[left:]...)
+	return result
+}
+
+// Добавляем скобки в умножение и деление
+func upgradePlusMinus(exp []byte) []byte {
+	result := make([]byte, 0)
+	left := 0
+	prevOperator := byte(0)
+	countBrackets := 0
+	for i := left; i < len(exp); i++ {
+		if exp[i] == '(' {
+			countBrackets++
+			result = append(result, exp[left:i]...)
+			result = append(result, '(')
+			array := upgradePlusMinus(exp[i+1:])
+			for countBrackets > 0 {
+				i++
+				if exp[i] == '(' {
+					countBrackets++
+				}
+				if exp[i] == ')' {
+					countBrackets--
+				}
+			}
+			i++
+			result = append(result, array...)
+			result = append(result, ')')
+			left = i
+			if i < len(exp) {
+				result = append(result, exp[i])
+				left = i + 1
+			}
+			prevOperator = 0
+			continue
+		}
+		if exp[i] == '+' {
+			if prevOperator == '*' || prevOperator == '/' {
+				result = append(result, exp[left:i+1]...)
+				left = i + 1
+				prevOperator = 0
+				continue
+			}
+			if prevOperator == 0 {
+				prevOperator = exp[i]
+				continue
+			}
+			result = append(result, '(')
+			result = append(result, exp[left:i]...)
+			result = append(result, ')', exp[i])
+			left = i + 1
+			prevOperator = 0
+		}
+		if exp[i] == '*' || exp[i] == '/' {
+			result = append(result, exp[left:i+1]...)
+			left = i + 1
+			prevOperator = exp[i]
+		}
+		if exp[i] == '-' {
+			if i == 0 || exp[i-1] < 48 {
+				continue
+			}
+			if prevOperator == '*' || prevOperator == '/' {
+				result = append(result, exp[left:i]...)
+				result = append(result, '+')
+				left = i
+				prevOperator = 0
+				continue
+			}
+			if prevOperator == 0 {
+				prevOperator = exp[i]
+				continue
+			}
+			result = append(result, '(')
+			result = append(result, exp[left:i]...)
+			result = append(result, ')', '+')
+			left = i
+			prevOperator = 0
+			continue
+		}
+		if exp[i] == ')' {
+			if prevOperator == '+' || prevOperator == '-' {
+				result = append(result, '(')
+				result = append(result, exp[left:i]...)
+				result = append(result, ')')
+				return result
+			}
+			result = append(result, exp[left:i]...)
+			return result
+		}
+	}
+	if prevOperator == '+' || prevOperator == '-' {
+		result = append(result, '(')
+		result = append(result, exp[left:]...)
+		result = append(result, ')')
+		return result
+	}
+	result = append(result, exp[left:]...)
+	return result
+}
+
+func updateDoubleMinus(exp []byte) []byte {
+	result := make([]byte, 0)
+	left := 0
+	for i := 0; i < len(exp)-1; i++ {
+		if exp[i] == '-' && exp[i+1] == '-' {
+			result = append(result, exp[left:i]...)
+			result = append(result, '+')
+			left = i + 2
+		}
 	}
 	result = append(result, exp[left:]...)
 	return result
--- Orchestrator/internal/tasks/arithmetic/upgrader_test.go
+++ Orchestrator/internal/tasks/arithmetic/upgrader_test.go
@@ -105,10 +105,139 @@ func Test_upgradeMultiDivide(t *testing.T) {
 			},
 			want: []byte("2+2-111+888*8/9*(2*2)+(599*33)*555+1"),
 		},
+		{
+			name: "multy with plus and brackets",
+			args: args{
+				exp: []byte("2+2-111+(888*89*2*2)+599*33*555+1"),
+			},
+			want: []byte("2+2-111+((888*89)*(2*2))+(599*33)*555+1"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := upgradeMultiDivide(tt.args.exp); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("upgradeMultiDivide() = %v, want %v", string(got), string(tt.want))
+			}
+		})
+	}
+}
+
+func Test_upgradePlusMinus(t *testing.T) {
+	type args struct {
+		exp []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			name: "simple plus",
+			args: args{
+				exp: []byte("1+2"),
+			},
+			want: []byte("(1+2)"),
+		},
+		{
+			name: "simple minus",
+			args: args{
+				exp: []byte("1-2"),
+			},
+			want: []byte("(1-2)"),
+		},
+		{
+			name: "4 plus",
+			args: args{
+				exp: []byte("2+2+2+2"),
+			},
+			want: []byte("(2+2)+(2+2)"),
+		},
+		{
+			name: "4 minus",
+			args: args{
+				exp: []byte("2-2-2-2"),
+			},
+			want: []byte("(2-2)+(-2-2)"),
+		},
+		{
+			name: "2 plus 1 minus",
+			args: args{
+				exp: []byte("2+2-2+2"),
+			},
+			want: []byte("(2+2)+(-2+2)"),
+		},
+		{
+			name: "1 plus 1 minus",
+			args: args{
+				exp: []byte("2+2-2"),
+			},
+			want: []byte("(2+2)+-2"),
+		},
+		{
+			name: "2 plus 1 minus",
+			args: args{
+				exp: []byte("2+2+2-2"),
+			},
+			want: []byte("(2+2)+(2-2)"),
+		},
+		{
+			name: "1 plus 2 minus",
+			args: args{
+				exp: []byte("2-2+2-2"),
+			},
+			want: []byte("(2-2)+(2-2)"),
+		},
+		{
+			name: "no brackets",
+			args: args{
+				exp: []byte("1+1/1+1+1*1+2"),
+			},
+			want: []byte("1+1/1+1+1*1+2"),
+		},
+		{
+			name: "negative numbers",
+			args: args{
+				exp: []byte("-1+1+-1+2"),
+			},
+			want: []byte("(-1+1)+(-1+2)"),
+		},
+		{
+			name: "with brackets",
+			args: args{
+				exp: []byte("-1+1+-1+(2*2)+(2+2+1)"),
+			},
+			want: []byte("(-1+1)+-1+(2*2)+((2+2)+1)"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := upgradePlusMinus(tt.args.exp); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("upgradeMultiDivide() = %v, want %v", string(got), string(tt.want))
+			}
+		})
+	}
+}
+
+func Test_upgradeDoubleMinus(t *testing.T) {
+	type args struct {
+		exp []byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want []byte
+	}{
+		{
+			name: "simple plus",
+			args: args{
+				exp: []byte("-1--2+2+2-2+-2+2--2"),
+			},
+			want: []byte("-1+2+2+2-2+-2+2+2"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := updateDoubleMinus(tt.args.exp); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("upgradeMultiDivide() = %v, want %v", string(got), string(tt.want))
 			}
 		})
