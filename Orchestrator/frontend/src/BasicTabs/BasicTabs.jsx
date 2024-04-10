695fe0624b979901e4f6402fb95f1a0e51a5c44a
Antonov Denis <den_ru1982@mail.ru>
2024-04-10 22:28:49 +0300

    Create jwt and user + front

--- Orchestrator/frontend/src/BasicTabs/BasicTabs.jsx
+++ Orchestrator/frontend/src/BasicTabs/BasicTabs.jsx
@@ -5,6 +5,9 @@ import Box from '@mui/material/Box';
 import CustomTabPanel from '../CustomTabPanel/CustomTabPanel';
 import TwoTabPanel from '../TwoTabPanel/TwoTabPanel';
 import ThreeTabPanel from '../ThreeTabPanel/ThreeTabPanel';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
   
   export default function BasicTabs({client}) {
     const [value, setValue] = React.useState(0);
@@ -15,6 +18,15 @@ import ThreeTabPanel from '../ThreeTabPanel/ThreeTabPanel';
   
     return (
       <Box sx={{ width: '100%' }}>
+        <Grid container justifyContent="center">
+          <Grid item container xs={12} alignItems="flex-end" direction="column">
+            <Grid item>
+              <Tooltip title="Add" placement="right-start">
+                <Button>Выход</Button>
+              </Tooltip>
+            </Grid>
+          </Grid>
+        </Grid>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
             <Tab label="Рассчитать выражение" />
