1c68a4196624176dc28fc2fbb56e66d104053fc1
Antonov Denis <den_ru1982@mail.ru>
2024-04-22 09:09:39 +0300

    Delete mess from agent

--- Agent/cmd/agent/main.go
+++ Agent/cmd/agent/main.go
@@ -8,14 +8,12 @@ import (
 
 	"github.com/adminsemy/yandexCalculator/Agent/intenal/config"
 	grpcclient "github.com/adminsemy/yandexCalculator/Agent/intenal/grpc_client"
-	"github.com/adminsemy/yandexCalculator/Agent/intenal/http/client"
 )
 
 func main() {
 	config := config.New()
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt)
-	go client.Ping(config)
 	for i := 0; i < config.MaxGoroutines; i++ {
 		go func(id int) {
 			for {
--- Agent/intenal/task/calculate/calculate.go
+++ Agent/intenal/task/calculate/calculate.go
@@ -1,9 +1,6 @@
 package calculate
 
 import (
-	"errors"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/adminsemy/yandexCalculator/Agent/intenal/entity/expression"
@@ -50,59 +47,4 @@ func CalculateGRPC(exp Expression) Expression {
 
 	}
 	return newExp
-}
-
-func Calculate(expression string) (string, error) {
-	array := strings.Split(expression, " ")
-	if len(array) != 3 {
-		return "", errors.New("неверное выражение")
-	}
-	id := array[0]
-	first, second, operation, err := parseExpression(array[1])
-	if err != nil {
-		return id, err
-	}
-	duration, err := strconv.ParseFloat(array[2], 64)
-	if err != nil {
-		return id, err
-	}
-	result := float64(0)
-	switch operation {
-	case "+":
-		time.Sleep(time.Duration(duration) * time.Second)
-		result = first + second
-	case "-":
-		time.Sleep(time.Duration(duration) * time.Second)
-		result = first - second
-	case "*":
-		time.Sleep(time.Duration(duration) * time.Second)
-		result = first * second
-	case "/":
-		time.Sleep(time.Duration(duration) * time.Second)
-		if second == 0 {
-			return id, errors.New("делить на ноль нельзя")
-		}
-		result = first / second
-	}
-	return id + " " + strconv.FormatFloat(result, 'f', -1, 64) + " " + array[2], nil
-}
-
-// Проверяем корректность значения
-func parseExpression(str string) (float64, float64, string, error) {
-	split := 0
-	for i := 1; i < len(str); i++ {
-		if str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' {
-			split = i
-			break
-		}
-	}
-	first, err := strconv.ParseFloat(str[:split], 64)
-	if err != nil {
-		return 0, 0, "", err
-	}
-	second, err := strconv.ParseFloat(str[split+1:], 64)
-	if err != nil {
-		return 0, 0, "", err
-	}
-	return first, second, string(str[split]), nil
 }
